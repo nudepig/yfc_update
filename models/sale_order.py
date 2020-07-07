ce3ae61d4369e2853950216ecea6c610c0e0deb5
auroral <1187774680@qq.com>
2020-07-07 17:18:22 +0800

    修改已知问题

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import json
+import datetime
+from odoo.http import request
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
